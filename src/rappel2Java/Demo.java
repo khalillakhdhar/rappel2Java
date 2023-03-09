99ce4e4c31c07d28470d1bf41573af7524b7715a
khalil <khalillakhdharatc@gmail.com>
2023-03-09 15:30:58 +0100

    code

--- src/rappel2Java/Demo.java
+++ src/rappel2Java/Demo.java
@@ -8,13 +8,18 @@ public class Demo {
 		// double > float > int
 		// => double =float ou int/  float=int 
 		int x=32;
+		x+=3; // x=x+3;
+		x++; // x+1
+		x--;// x=x-1
+		y-=1.45; // y=y-1.45
 		int z=(int) y; 
 		// casting de y dans z (prendre l'entier)
 		String valeur="341";
-		Integer.parseInt(valeur); // convertir valeur "341" en entier
+		int w=Integer.parseInt(valeur); // convertir valeur "341" en entier
 		Double.parseDouble(valeur); // en double
 		Float.parseFloat(valeur); // en float
-		
+		boolean b=true; // valeur directe => true ou false
+		b=x>y; // expression logique => évaluer en true ou false
 
 	}
 
